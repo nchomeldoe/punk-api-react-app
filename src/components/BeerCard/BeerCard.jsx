6286306cfb2d1f5fb1a40c90a1d66b12555f7fbd
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-07-22 12:25:50 +0100

    started styling cards

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -13,22 +13,24 @@ const BeerCard = ({ beer }) => {
   } = beer;
   return (
     <div className="beer-card">
-      <img className="beer-card__image" src={image_url} alt={name} />
-      <h3 className="beer-card__name">{name}</h3>
-      <h4 className="beer-card__tagline">{tagline}</h4>
-      <p className="beer-card__abv-ph">
-        ABV: {abv} | pH: {ph}
-      </p>
-      <p className="beer-card__first-brewed">Since: {first_brewed}</p>
-      <p className="beer-card__first-brewed">{description}</p>
-      <h5 className="beer-card__pairings-header">Pair with:</h5>
-      <ul className="beer-card__pairings">
-        {food_pairing?.map((dish, index) => (
-          <li key={index} className="beer-card__pairing">
-            {dish}
-          </li>
-        ))}
-      </ul>
+      <div className="beer-card__content-container">
+        <img className="beer-card__image" src={image_url} alt={name} />
+        <h3 className="beer-card__name">{name}</h3>
+        <h4 className="beer-card__tagline">{tagline}</h4>
+        <p className="beer-card__abv-ph">
+          ABV: {abv} | pH: {ph}
+        </p>
+        <p className="beer-card__first-brewed">Since: {first_brewed}</p>
+        <p className="beer-card__first-brewed">{description}</p>
+        <h5 className="beer-card__pairings-header">Pair with:</h5>
+        <ul className="beer-card__pairings">
+          {food_pairing?.map((dish, index) => (
+            <li key={index} className="beer-card__pairing">
+              {dish}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -5,13 +5,9 @@ const Main = ({ beers }) => {
   return (
     <div className="main">
       <div className="main__beer-cards">
-        {/* {beers.map((beer) => (
+        {beers?.map((beer) => (
           <BeerCard beer={beer} key={beer.id} />
-        ))} */}
-        {beers.map((beer) => {
-          // console.log(beer);
-          return <BeerCard beer={beer} key={beer.id} />;
-        })}
+        ))}
       </div>
     </div>
   );
