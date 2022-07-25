71a292d89aae9267da7568d6c94c6a66b3a227f5
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-07-25 13:31:15 +0100

    wrote function to fetch all beersfrom api across all available pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
--- src/services/beer.service.js
+++ src/services/beer.service.js
@@ -2,12 +2,23 @@ const API_URL = "https://api.punkapi.com/v2/beers";
 
 const getBeers = async () => {
   try {
-    const response = await fetch(`${API_URL}?per_page=80`);
-    if (!response.ok) {
-      throw new Error(response.status + " error with request");
+    let page = 0;
+    let allBeerData = [];
+    while (1) {
+      page++;
+      let response = await fetch(`${API_URL}?page=${page}&per_page=80`);
+      if (!response.ok) {
+        throw new Error(response.status + " error with request");
+      }
+      let newBeerData = await response.json();
+      if (newBeerData.length) {
+        allBeerData.push(newBeerData);
+      } else {
+        break;
+      }
+      console.log("all", allBeerData);
     }
-    const beerData = await response.json();
-    return beerData;
+    return allBeerData.flat();
   } catch (error) {
     return error.message;
   }
