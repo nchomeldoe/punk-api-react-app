8dd463c92f8df30a4fe8d1844ae8b0b4dbb50bdb
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-07-30 14:49:33 +0100

    wrote first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+it("should render the app with the title BrewDog", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/brewdog/i);
+  expect(heading).toBeInTheDocument();
 });
